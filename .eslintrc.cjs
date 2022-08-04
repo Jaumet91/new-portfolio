d81168b4eb347dc30eded1c6e7f1a0d896f81300
Jaime Cortes <hello@jaimecortesdev.com>
2022-08-04 09:07:17 +0200

    Linter and Formatter installed

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'eslint-config-prettier',
+    'plugin:react/jsx-runtime'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'multiline-ternary': 'off',
+    'no-unexpected-multiline': 'error',
+    indent: 'off'
+  }
+}
