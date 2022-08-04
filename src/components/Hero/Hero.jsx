e22238d8bd5bcd435bd90d5f88c5954f5698d3f7
Jaime Cortes <hello@jaimecortesdev.com>
2022-08-04 19:20:12 +0200

    added aos module

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,8 +2,39 @@ import { bgMain } from '../../assets/images';
 
 export const Hero = () => {
   return (
-    <div className='header__bg'>
-      <img className='header__pic' src={bgMain} />
-    </div>
+    <>
+      <div className='header__bg'>
+        <img className='header__pic' src={bgMain} />
+      </div>
+      <div className='center'>
+        <div className='header__wrap'>
+          <div
+            className='header__stage stage aos-init aos-animate'
+            data-aos='transform'>
+            Jaime Cortes
+          </div>
+          <div className='header__title title h1'>
+            <span
+              data-aos='transform'
+              data-aos-delay='150'
+              className='aos-init aos-animate'>
+              Hola!, me
+            </span>
+            <span
+              data-aos='transform'
+              data-aos-delay='300'
+              className='aos-init aos-animate'>
+              llamo Jaime y soy
+            </span>
+            <span
+              data-aos='transform'
+              data-aos-delay='450'
+              className='aos-init aos-animate'>
+              Full-Stack Developer
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
