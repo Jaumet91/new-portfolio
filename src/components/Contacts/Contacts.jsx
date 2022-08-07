d063d683be8093250208d7ebf9645bde98862a0c
Jaime Cortes <hello@jaimecortesdev.com>
2022-08-07 09:06:07 +0200

    Add accesibility for screen readers at ContactForm

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,38 +1,7 @@
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
-
-import {
-  Email,
-  Place,
-  Phone,
-  Send,
-  Profile,
-  // Arrows,
-  Message,
-  ArrowRight,
-  LinkedIn,
-  Github
-} from '../icons';
-import { Figures } from './';
+import { ContactForm, Figures } from './';
+import { Email, Place, Phone, LinkedIn, Github } from '../icons';
 
 export const Contacts = () => {
-  const formik = useFormik({
-    initialValues: {
-      name: '',
-      email: '',
-      message: ''
-    },
-    validationSchema: Yup.object({
-      name: Yup.string().required(true),
-      email: Yup.string().email(true),
-      message: Yup.string().required(true)
-    }),
-    onSubmit: () => formValue => {
-      console.log('Formulario enviado');
-      console.log(formValue);
-    }
-  });
-
   return (
     <article className='contacts'>
       <section className='contacts__center center'>
@@ -75,79 +44,7 @@ export const Contacts = () => {
                 <div className='contacts__text'>+34 609 431 585</div>
               </a>
             </div>
-            <form
-              onSubmit={formik.handleSubmit}
-              className='contacts__form'
-              data-aos='animation-translate-y'
-              data-aos-delay='200'>
-              <div className='contacts__field field'>
-                <div className='field__wrap'>
-                  <input
-                    className='field__input'
-                    type='text'
-                    name='name'
-                    onChange={formik.handleChange}
-                    value={formik.values.name}
-                    error={formik.errors.name}
-                    placeholder='Nombre'
-                  />
-                  <div className='field__icon'>
-                    <Profile className='icon' height={'20'} width={'20'} />
-                  </div>
-                </div>
-              </div>
-              <div className='contacts__field field'>
-                <div className='field__wrap'>
-                  <input
-                    className='field__input'
-                    type='email'
-                    name='email'
-                    onChange={formik.handleChange}
-                    value={formik.values.email}
-                    error={formik.errors.email}
-                    placeholder='Correo'
-                  />
-                  <div className='field__icon'>
-                    <Send className='icon' height={'20'} width={'20'} />
-                  </div>
-                </div>
-              </div>
-              {/* <div className='contacts__field field'>
-                <div className='field__wrap'>
-                  <select className='field__select' name='question'>
-                    <option>Opcion 1</option>
-                    <option>Opcion 2</option>
-                  </select>
-                  <div className='field__icon'>
-                    <Arrows className='icon' height={'20'} width={'20'} />
-                  </div>
-                </div>
-              </div> */}
-              <div className='contacts__field field'>
-                <div className='field__wrap'>
-                  <textarea
-                    className='field__textarea'
-                    name='message'
-                    onChange={formik.handleChange}
-                    value={formik.values.message}
-                    error={formik.errors.message}
-                    placeholder='Cuéntame tus ideas'
-                  />
-                  <div className='field__icon'>
-                    <Message className='icon' height={'20'} width={'20'} />
-                  </div>
-                </div>
-              </div>
-              <div className='contacts__btn'>
-                <button
-                  className='btn btn_purple'
-                  type='submit'
-                  disabled={formik.isSubmitting}>
-                  <span className='btn__text'>Hablemos</span>
-                  <ArrowRight className='icon' height={'20'} width={'20'} />
-                </button>
-              </div>
-            </form>
+            <ContactForm />
           </div>
           <div className='contacts__social'>
             <div className='contacts__box' data-aos='animation-translate-y'>
--- src/components/Contacts/index.jsx
+++ src/components/Contacts/index.jsx
@@ -1,2 +1,3 @@
 export * from './Contacts';
 export * from './Figures';
+export * from './ContactForm';
